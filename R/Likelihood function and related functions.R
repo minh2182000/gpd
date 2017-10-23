9d8b857a8b52848122baae550148372fdd8f635b
minh2182000 <minhpham@mail.usf.edu>
2017-10-23 19:52:27 -0400

    Update

--- R/Likelihood function and related functions.R
+++ R/Likelihood function and related functions.R
@@ -1,6 +1,8 @@
 #--------- Log likelihood function ------------------------------------------------- ----
     L = function(k,alpha){
+      with(parent.env(environment()),
       sum(-delta*log(alpha)+(1/k-delta)*log(1-k/alpha*x))
+      )
     }
 #-------- k estimation from theta0 ------------------------------------------------- ----
     # note: (theta=k/alpha)
@@ -8,19 +10,25 @@
     k = function(theta0){
       if (!exists('r', envir = parent.env(environment()))){stop("this function is being run outside of the mother's function 'mle'")}
       r = get('r', envir = parent.env(environment()))
-      return(-1/r*sum(log(1-theta0*x)))
+      with(parent.env(environment()),
+        return(-1/r*sum(log(1-theta0*x)))
+      )
     }
 #------- alpha estimation from theta0 ---------------------------------------------- ----
     alpha = function(theta0){
       k(theta0)/theta0
-    }			
+    }
 #------- function h(theta)--------------------------------------------------------- ----
-    h = function (theta){				   
-      1/n*sum(log(1-theta*x))*1/r*sum((1-theta*x[1:r])^-1) + 1/n*sum((1-theta*x)^-1)-1
+    h = function (theta){
+      with(parent.env(environment()),
+        1/n*sum(log(1-theta*x))*1/r*sum((1-theta*x[1:r])^-1) + 1/n*sum((1-theta*x)^-1)-1
+      )
     }
 #------- function h'(theta) ------------------------------------------------------- ----
     h_prime = function(theta){
-      1/n*sum(-x/(1-theta*x))*1/r*sum((1-theta*x[1:r])^-1) + 
-      1/n*sum(log(1-theta*x))*1/r*sum(x[1:r]*(1-theta*x[1:r])^-2) +
-      1/n*sum(x*(1-theta*x)^-2)
+      with(parent.env(environment()),
+        1/n*sum(-x/(1-theta*x))*1/r*sum((1-theta*x[1:r])^-1) +
+        1/n*sum(log(1-theta*x))*1/r*sum(x[1:r]*(1-theta*x[1:r])^-2) +
+        1/n*sum(x*(1-theta*x)^-2)
+      )
     }
